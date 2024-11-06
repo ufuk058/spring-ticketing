abff80180b0dd032ee6f9f0c198f572548467855
ufuk058 <ufuksahinduran.uk@gmail.com>
2024-11-06 14:09:54 +0000

    Map Object corrected

--- src/main/java/com/ticketing/service/impl/AbstractMapService.java
+++ src/main/java/com/ticketing/service/impl/AbstractMapService.java
@@ -7,14 +7,8 @@ import java.util.stream.Collectors;
 
 public abstract class AbstractMapService <T, ID>  {
 
-    public Map<ID,T> map2= new LinkedHashMap<>(); //This is our database
-    LinkedHashMap<ID,T> map=map2.entrySet()
-            .stream().sorted((entry1,entry2) -> -1).
-            collect(Collectors.toMap(Map.Entry:: getKey,Map.Entry::getValue,(e1,e2)->e1,LinkedHashMap::new));
+    public Map<ID,T> map= new LinkedHashMap<>(); //This is our database
 
-    private void reverseOrder(){
-
-    }
 
     T save (ID id, T object){
         return map.put(id,object);
